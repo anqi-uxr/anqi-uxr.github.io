d539e1beaed5bb6b1f97b8427475c2978fa51975
Anqi Li <anqi.li.uxr@gmail.com>
2021-03-13 20:25:03 -0600

    fix imports

--- src/components/project/ProjectHeader.js
+++ src/components/project/ProjectHeader.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "../../styles/project-header.scss"
 
 const ProjectHeader = ({ title, subtitle, background }) => {
   return (
     <div className="project-header">
       <img src={background}></img>
-      <div class="title-section">
+      <div className="title-section">
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
       </div>
