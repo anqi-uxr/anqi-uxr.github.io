906b4ddac46d50c6e4399569226d53667fda7144
Anqi Li <anqi.li.uxr@gmail.com>
2021-03-13 20:45:26 -0600

    [data] separate data from layout

---
+++ src/data/hivetec-data.js
@@ -0,0 +1,10 @@
+import hivetec from "../images/hivetec.jpg"
+
+export default {
+  siteTitle: "IoT for Agriculture",
+  header: {
+    title: "Remote Beehive Monitoring Technology",
+    subtitle: "Care for the BEEs & promote species diversity",
+    background: hivetec,
+  },
+}
--- src/pages/hivetec.js
+++ src/pages/hivetec.js
@@ -3,17 +3,17 @@ import React from "react"
 import SEO from "../components/main/seo"
 import ProjectHeader from "../components/project/ProjectHeader"
 
-import hivetec from "../images/hivetec.jpg"
+import data from "../data/hivetec-data"
 
-const IndexPage = () => (
+const HiveTec = () => (
   <>
-    <SEO title="IoT for Agriculture" />
+    <SEO title={data.siteTitle} />
     <ProjectHeader
-      title="Remote Beehive Monitoring Technology"
-      subtitle="Care for the BEEs & promote species diversity"
-      background={hivetec}>
+      title={data.header.title}
+      subtitle={data.header.subtitle}
+      background={data.header.background}>
     </ProjectHeader>
   </>
 )
 
-export default IndexPage
+export default HiveTec
