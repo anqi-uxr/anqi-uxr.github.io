6d0d9f6cc178b19391844423cf50d1b24796c12f
Anqi Li <anqi.li.uxr@gmail.com>
2021-03-13 20:46:07 -0600

    [data] rename data import

--- src/pages/hivetec.js
+++ src/pages/hivetec.js
@@ -3,15 +3,15 @@ import React from "react"
 import SEO from "../components/main/seo"
 import ProjectHeader from "../components/project/ProjectHeader"
 
-import data from "../data/hivetec-data"
+import hivetec_data from "../data/hivetec-data"
 
 const HiveTec = () => (
   <>
-    <SEO title={data.siteTitle} />
+    <SEO title={hivetec_data.siteTitle} />
     <ProjectHeader
-      title={data.header.title}
-      subtitle={data.header.subtitle}
-      background={data.header.background}>
+      title={hivetec_data.header.title}
+      subtitle={hivetec_data.header.subtitle}
+      background={hivetec_data.header.background}>
     </ProjectHeader>
   </>
 )
