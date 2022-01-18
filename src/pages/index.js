e87c482cdd11ad546417dc4daf9000e9f65f8056
Qingwei Lan <qingweilan@gmail.com>
2022-01-17 22:11:06 -0600

    index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,21 @@
 import React from 'react'
-import ReactGA from 'react-ga'
 
-import ButtonLink from 'components/ButtonLink'
 import Layout from 'components/Layout'
 import SEO from 'components/Seo'
 import GridTemplate from 'components/GridTemplate'
-import BlogList from 'components/BlogList'
-import Author from 'components/Author'
-
-const trackLanguageClick = () => {
-  ReactGA.event({
-    category: 'Home',
-    action: 'click',
-    label: `Home - Go to english`
-  })
-}
+import Content from 'components/Content'
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title='Home' />
       <GridTemplate>
-        <ButtonLink
-          to="/en/"
-          title="View in english"
-          onClick={() => trackLanguageClick()}>
-          View in english
-        </ButtonLink>
-
-        <Author />
-        <BlogList />
+        <Content>
+          <h1>Hello 👋</h1>
+          <p>I'm a UX researcher with 2+ years of experience in UX research with a background training in psychology and human factors.</p>
+          <p>I'm passionate about understanding people (not just users), building trust between people and technology, and most importantly make people's lives better through my research.</p>
+          <p>I am also a fast learner who is always looking for opportunities to grow as a researcher, a team player, and someone who makes people's lives better through UX research.</p>
+        </Content>
       </GridTemplate>
     </Layout>
   )
