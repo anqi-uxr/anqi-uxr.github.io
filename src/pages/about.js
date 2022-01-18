ae8bd71e42d88aa15c24f95c7929ddc849041405
Qingwei Lan <qingweilan@gmail.com>
2022-01-17 22:19:17 -0600

    about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,21 @@
 import React from 'react'
-import ReactGA from 'react-ga'
 
-import ButtonLink from 'components/ButtonLink'
 import Layout from 'components/Layout'
 import SEO from 'components/Seo'
 import GridTemplate from 'components/GridTemplate'
-import Author from 'components/Author'
-
-const trackLanguageClick = () => {
-  ReactGA.event({
-    category: 'About',
-    action: 'click',
-    label: `About - Go to english`
-  })
-}
+import Content from 'components/Content'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <SEO title='Um pouco de mim' />
+      <SEO title='About Me' />
       <GridTemplate>
-        <ButtonLink
-          to="/en/hi/"
-          title="View in english"
-          onClick={() => trackLanguageClick()}>
-            View in english
-        </ButtonLink>
-
-        <Author fullText={true} />
+        <Content>
+          <h1>More about me</h1>
+          <p>I'm a UX researcher with a background in psychology and behavior research. I am a human-enthusiastic. I enjoy studying people, diving into people's lives and minds, understanding their lifestyles, experiences, and motivations.</p>
+          <p>I care deeply about making meaningful social impacts through my research. You can read more in detail in my case studies how my research benefits beyond the scope of a product and reach into promoting species diversity through caring about honeybees, raising open science awareness, and equalizing student access to educational resources.</p>
+          <p>Outside of work, I enjoy spending time with my kitties😼, developing recipes in my kitchen👨‍🍳‍, and cycling 🚴‍♀️.</p>
+        </Content>
       </GridTemplate>
     </Layout>
   )
