ede3df06fb4f537c84c7f3956053062b9a1ab86c
Qingwei Lan <qingweilan@gmail.com>
2022-01-26 22:50:28 -0600

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,8 @@ const IndexPage = () => {
       <SEO title='About Me' />
       <GridTemplate>
         <Content>
-          <h1>More about me</h1>
+          <h1>Hi there👋 Nice to meet you!</h1>
+          <p>My name is Anqi (安琪)</p>
           <p>I'm a UX researcher with a background in psychology and behavior research. I am a human-enthusiastic. I enjoy studying people, diving into people's lives and minds, understanding their lifestyles, experiences, and motivations.</p>
           <p>I care deeply about making meaningful social impacts through my research. You can read more in detail in my case studies how my research benefits beyond the scope of a product and reach into promoting species diversity through caring about honeybees, raising open science awareness, and equalizing student access to educational resources.</p>
           <p>Outside of work, I enjoy spending time with my kitties😼, developing recipes in my kitchen👨‍🍳‍, and cycling 🚴‍♀️.</p>
