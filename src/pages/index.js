3688b210137437d552f0bcacbc0053587a608239
Qingwei Lan <qingweilan@gmail.com>
2023-06-19 22:28:41 -0500

    update desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,15 @@ const IndexPage = () => {
       <GridTemplate>
         <Content>
           <h1>Hello 👋</h1>
-          <p>I'm a UX researcher with 2+ years of experience in UX research with a background training in psychology and human factors.</p>
-          <p>I'm passionate about understanding people (not just users), building trust between people and technology, and most importantly make people's lives better through my research.</p>
-          <p>I am also a fast learner who is always looking for opportunities to grow as a researcher, a team player, and someone who makes people's lives better through UX research.</p>
-          <p><center>
+          <p>I'm a UX researcher and product strategist with a background in psychology and human factors. I distinguish myself through diligent research, strategic thinking, entrepreneurial mindset.</p>
+          <p>I enjoy navigating ambiguous problem spaces, solving complex problems with simple solutions, and drive innovation through actionable insights.</p>
+          <p>My personal philosophy in UXR:</p>
+          <ul>
+            <li>Research should be rigorous but also bold: years of experience being in startups and answering ambiguous questions have taught me to be resourceful, innovative, and goal-driven</li>
+            <li>Research goes further with great collaboration: always be empathic, insightful, and action-driven with not just users but also partners. Problem gets solved by a team.</li>
+            <li>Never be afraid to share, lead, and innovate: UXR has the best understanding of users and often great intuitions. Research enables me to be proactive in driving product innovation and taking strategic lead</li>
+          </ul>
+          <p>
             Check out my{' '}
             <AniLink
               cover
@@ -27,10 +32,10 @@ const IndexPage = () => {
               duration={1}
               to='/experience/'
             >
-              past experience
+              previous work
             </AniLink>
-            !
-          </center></p>
+            . Please reach out directly to discuss more of my recent work.
+          </p>
         </Content>
       </GridTemplate>
     </Layout>
